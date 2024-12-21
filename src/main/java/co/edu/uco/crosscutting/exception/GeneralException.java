1bddf3f64c9457c62056876c2f8c94405a7681cf
juan.cardona <felipe7u7432@gmail.com>
2024-12-21 14:50:45 -0500

    :sparkles: feat(exception): Add specific exception CrossCuttingException
    Issue: #5

--- src/main/java/co/edu/uco/crosscutting/exception/GeneralException.java
+++ src/main/java/co/edu/uco/crosscutting/exception/GeneralException.java
@@ -1,20 +1,23 @@
 package co.edu.uco.crosscutting.exception;
 
+import java.io.Serial;
+
 import static co.edu.uco.crosscutting.util.UtilObject.getDefaultIsNull;
 import static co.edu.uco.crosscutting.util.UtilText.trim;
 
 public class GeneralException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = -3393801537376736983L;
     private String userMessage;
     private String technicalMessage;
     private Exception rootException;
-    public static GeneralException build(String technicalMessage) {
+    public static GeneralException build(final String technicalMessage) {
         return new GeneralException(null, technicalMessage, null);
     }
-    public static GeneralException build(String technicalMessage, Exception rootException) {
+    public static GeneralException build(final String technicalMessage, Exception rootException) {
         return new GeneralException(null, technicalMessage, rootException);
     }
-    protected GeneralException(String userMessage, String technicalMessage, Exception rootException) {
+    protected GeneralException(final String userMessage, final String technicalMessage, Exception rootException) {
         super();
         setUserMessage(userMessage);
         setTechnicalMessage(technicalMessage);
